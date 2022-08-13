f13562950e79a115fc406007d643ae7ce96ac28e
Aadi-Sonwane <aadus.info@gmail.com>
2022-08-13 07:17:26 -0700

    inintal commit of this project

---
+++ inc/footer.php
@@ -0,0 +1,56 @@
+</body>
+
+<!-- Footer -->
+	<section id="footer">
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Quick links</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Quick links</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Quick links</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="javascript:void();"><i class="fa fa-angle-double-right"></i>About</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+					<ul class="list-unstyled list-inline social text-center">
+						<li class="list-inline-item"><a href="https://www.facebook.com/profile.php?id=100015127320336"target="_blank"><i class="fa fa-facebook"></i></a></li>
+						<li class="list-inline-item"><a href="https://twitter.com/aadu_1"target="_blank"><i class="fa fa-twitter"></i></a></li>
+						<li class="list-inline-item"><a href="https://www.instagram.com/aadi_sonwane/"target="_blank"><i class="fa fa-instagram"></i></a></li>
+						<!-- <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>-->
+						<li class="list-inline-item"><a href="https://www.linkedin.com/in/aditya-sonwane-317365210/"target="_blank"><i class="fa fa-linkedin"></i></a></li>
+						<li class="list-inline-item"><a href="https://github.com/Aadi-Sonwane" target="_blank"><i class="fa fa-github"></i></a></li>
+					</ul>
+				</div>
+			</div>	
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                    <p class="h6">&copy <a href="">techbuild.tech</a>. Developed by Aditya Sonwane</a></p>
+				</div>
+				</hr>
+			</div>	
+		</div>
+	</section>
+  <!-- ./Footer -->
+  
+
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</footer>
